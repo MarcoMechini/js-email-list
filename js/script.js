60394965b54d69c623f2f615dec12bc4c67359e1
Marco Mechini <mechinimarco35@gmail.com>
2024-11-18 17:08:41 +0100

    feat: bonus complete

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 const albumEmail = document.querySelector(".album-email")
+const buttonNewEmail = document.querySelector("button")
 
 
+const getEmail = () => {
 
-for (let i = 1; i <= 10; i++) {
+    albumEmail.innerHTML = '';
+    for (let i = 1; i <= 10; i++) {
 
-    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
-        const email = resp.data.response;
-        albumEmail.innerHTML += `<li>${i} ${email}</li>`
+        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
+            const email = resp.data.response;
+            albumEmail.innerHTML += `<li>${email}</li>`
 
-    });
-
+        });
+    }
 }
+
+getEmail();
+buttonNewEmail.addEventListener("click", getEmail);
+
